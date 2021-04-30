2c09b31517bb7a96f2b8b1272fac4f52e3a34b8f
MVictorL <mvictorl@gmail.com>
2021-04-30 08:52:09 +0300

    Start adding Roles one-2-many for User

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const path = require('path')
 const routes = require('./routes')
 const db = require('./db.config')
+const errorMiddleware = require('./middleware/errorMiddleware')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.json())
-app.use('/public', express.static('static'))
+app.use('/public', express.static(path.resolve(__dirname, 'static')))
 app.use('/api', routes)
+app.use(errorMiddleware)
 
+// TODO: delete test route below
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'All right!' })
 })
